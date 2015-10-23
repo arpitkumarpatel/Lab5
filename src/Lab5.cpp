b06d77fac6a69f0c9006f37a47d77e526ddc3b9f
arpitkumarpatel <arpitkumar.patel@ryerson.ca>
2015-10-23 16:40:39 -0400

    Added a new line to the project that prints out my name.

---
+++ src/Lab5.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Lab5.cpp
+// Author      : Arpit Patel
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "" << endl; // prints 
+	cout << "My name is Arpit \n";
+	return 0;
+}
